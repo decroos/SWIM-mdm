9adfbb557e1ba08a87fa9ccf57306d2a6782cf22
Didier Decroos <decroos@fr.ibm.com>
2020-01-14 17:25:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('swimassetmanager.html', url=url)
+    return render_template('swimassetmanager.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
