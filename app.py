15779acbca3ea3ea76d224db76d71efdc7012593
Didier Decroos <decroos@fr.ibm.com>
2020-03-13 19:35:48 +0100

    add all apps

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('swim-mdm.html')
 
+@app.route('/Artemis')
+def artemis():
+    return render_template('/Artemis/artemis.html')
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
